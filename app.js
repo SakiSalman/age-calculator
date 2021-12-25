3114e8517856c9ad6c92e8a34f69ff9794a6d990
sakisalman <codersaki98@gmail.com>
2021-12-26 03:25:12 +0600

    Updated

--- app.js
+++ app.js
@@ -2,27 +2,25 @@ const name = document.querySelector('#name');
 const year = document.querySelector('#year');
 const check_btn = document.querySelector('#check_btn');
 const result = document.querySelector('#result');
-
-
+const h2 = document.querySelector('h2');
 
 check_btn.addEventListener('click', () => {
 
+
+
      if (name.value == "" || year.value == "") {
-          
-          return result.innerHTML = '<p class="alert alert-danger">Hi, all fields are required*</p>';
-
 
           
-     
-               
-     
+          h2.className = ('text-danger');
+          
+          result.innerHTML = '<p class="alert alert-danger">Hi, all fields are required*</p>';
      }else{
                result.innerHTML = ageCal(name.value, year.value);
                name.value = '';
                year.value = '';
-
+               h2.className = ('text-success');
 }
-     
+   
 });
 
 
